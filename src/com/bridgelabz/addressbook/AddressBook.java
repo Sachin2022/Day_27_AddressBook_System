83c7dae9e09170f341fbb8a9afb5b98246ebdd4a
Sachin2022 <sachikatarki1995@gmail.com>
2022-03-09 15:41:16 +0530

    Added:welcome to the Address Book System

---
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.addressbook;
+
+import java.util.List;
+
+public class AddressBook {
+	private List<Contact> Contacts;
+
+	public List<Contact> getContacts() {
+		return Contacts;
+	}
+
+	public void setContacts(List<Contact> Contacts) {
+		this.Contacts = Contacts;
+	}
+
+}
---
+++ src/com/bridgelabz/addressbook/AddressBookMainclass.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookMainclass {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To address Book Programme");
+	}
+
+}
